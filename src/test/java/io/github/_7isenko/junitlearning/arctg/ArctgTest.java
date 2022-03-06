6ffaf99be45324ed39a1778ac5e88247453c7fc1
7isenko <7isenko.max@gmail.com>
2022-03-06 20:53:59 +0300

    changed test name

--- src/test/java/io/github/_7isenko/junitlearning/arctg/ArctgTest.java
+++ src/test/java/io/github/_7isenko/junitlearning/arctg/ArctgTest.java
@@ -11,14 +11,14 @@ class ArctgTest {
     private final Arctg arctg = new Arctg(0.00001);
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/arctg/common_data.csv")
-    void arctg_ShouldReturnExpectedValuesOnCommonPoints(double input, double expected) {
+    @CsvFileSource(resources = "/arctg/good_data.csv")
+    void arctg_ShouldReturnExpectedValuesOnGoodPoints(double input, double expected) {
         Assertions.assertEquals(expected, arctg.arctg(input), arctg.getEpsilon());
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/arctg/bad_data.csv")
-    void arctg_ShouldThrowIllegalArgumentExceptionOnBadValues(double input) {
+    void arctg_ShouldThrowIllegalArgumentExceptionOnBadPoints(double input) {
         Assertions.assertThrows(IllegalArgumentException.class, ()-> arctg.arctg(input));
     }
 }
