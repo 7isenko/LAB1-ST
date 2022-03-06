a7b17031fe90bc60a91f220535701b2c4510c750
7isenko <7isenko.max@gmail.com>
2022-03-07 00:23:09 +0300

    simple radix sort test

---
+++ src/main/java/io/github/_7isenko/junitlearning/radix/RadixSort.java
@@ -0,0 +1,57 @@
+package io.github._7isenko.junitlearning.radix;
+
+import java.util.Arrays;
+
+/**
+ * @author 7isenko
+ */
+public class RadixSort {
+
+    public void sort(int[] array) throws IllegalArgumentException {
+
+        // Checking
+        if (array == null) {
+            throw new IllegalArgumentException("Array is null.");
+        }
+
+        if (array.length == 0) {
+            return;
+        }
+
+        if (Arrays.stream(array).min().getAsInt() < 0) {
+            throw new IllegalArgumentException("Radix sort works only with positive numbers.");
+        }
+
+        // Sorting
+        int max = Arrays.stream(array).max().getAsInt();
+        int length = String.valueOf(max).length();
+
+        int[][] bucket = new int[10][10];
+        int[] bucketCount = new int[10];
+
+        int divisor = 1;
+
+        // Initialize the buckets
+        for (int pass = 0; pass < length; pass++) {
+
+            for (int i = 0; i < array.length; i++) {
+                bucketCount[i] = 0;
+            }
+
+            // sort the numbers according to the digit at passth place
+            for (int value : array) {
+                int remainder = (value / divisor) % 10;
+                bucket[remainder][bucketCount[remainder]] = value;
+                bucketCount[remainder] += 1;
+            }
+
+            // collect the numbers after PASS pass
+            for (int k = 0, i = 0; k < 10 && i < array.length; k++) {
+                for (int j = 0; j < bucketCount[k]; j++, i++) {
+                    array[i] = bucket[k][j];
+                }
+            }
+            divisor *= 10;
+        }
+    }
+}
---
+++ src/test/java/io/github/_7isenko/junitlearning/radix/RadixSortTest.java
@@ -0,0 +1,43 @@
+package io.github._7isenko.junitlearning.radix;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author 7isenko
+ */
+class RadixSortTest {
+
+    private RadixSort radixSort = new RadixSort();
+
+    @Test
+    void sort() {
+        int[] arr = {1000, 1, 4, 6, 100, 14, 2};
+        int[] sorted = {1, 2, 4, 6, 14, 100, 1000};
+
+        radixSort.sort(arr);
+        assertArrayEquals(sorted, arr);
+    }
+
+    @Test
+    void sort_ShouldThrowExceptionForNegativeValues() {
+        int[] arr = {1, 4, -6, 0, 100};
+        assertThrows(IllegalArgumentException.class, () -> radixSort.sort(arr));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    void sort_ShouldThrowExceptionForNullArray(int[] arr) {
+        assertThrows(IllegalArgumentException.class, () -> radixSort.sort(arr));
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    void sort_ShouldExecuteForEmptyArray(int[] arr) {
+        assertArrayEquals(arr, arr);
+    }
+}
